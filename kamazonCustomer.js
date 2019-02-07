d76559899eb84877ad7cfcb0277b9132ee9afb99
xkeekyx <xkeyanna@yahoo.com>
2019-02-07 15:43:22 -0800

    updated files loaded

---
+++ kamazonCustomer.js
@@ -0,0 +1,110 @@
+// Dependencies
+var mysql = require('mysql');
+var wrap = require('word-wrap');
+var Table = require('cli-table');
+var inquirer = require('inquirer');
+var colors = require('colors');
+
+// sets connection param for database connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    // sets username
+    user: "root",
+    // sets password
+    password: "Happy1day",
+    // sets current database
+    database: "kamazon_db"
+});
+
+// makes connection with the server
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    itemsForSale();
+});
+
+// A query which returns all items available for sale ().
+function itemsForSale() {
+    connection.query("SELECT item_id, product_name, price, department_name FROM products WHERE price > 0;", function (err, result) {
+        // gets and builds the table header
+        var obj = result[0];
+        var header = [];
+        for (var prop in obj) {
+            header.push(prop);
+        }
+
+        // instantiate 
+        var table = new Table({
+            head: header,
+            colWidths: [20, 55, 10, 20]
+        });
+
+        // gets and sets the data in the table
+        var item_ids = [];
+        for (var i = 0; i < result.length; i++) {
+            item_ids.push(result[i].item_id);
+            table.push([result[i].item_id, wrap(result[i].product_name), result[i].price.toFixed(2), result[i].department_name]);
+        }
+        var output = table.toString();
+        console.log(output);
+        purchaseItem(item_ids);
+    });
+}
+
+// sets function for cutomer to make a purchase
+// list gets the items id's as an array and passed to the promt/choices param
+function purchaseItem(list) {
+    inquirer
+        .prompt([{
+            name: "buy",
+            type: "list",
+            message: "Please indicate which item would you like to purchase?",
+            choices: list
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "Please enter quantity?",
+        }])
+        .then(function (answer) {
+            // sets a query to select the item the user has chosen
+            var query = "SELECT item_id, stock_quantity, price FROM products WHERE ?";
+            connection.query(query, { item_id: answer.buy }, function (err, res) {
+                // console.log(res);
+                var inputQuantity = answer.quantity;
+                checkStock(res[0].stock_quantity, inputQuantity, res[0].price.toFixed(2), res[0].item_id);
+            });
+        })
+}
+
+// checks quantity against the stock
+function checkStock(on_stock, buy_quantity, price, item_id) {
+    if (on_stock >= buy_quantity) {
+        var total_price = buy_quantity * price;
+        console.log(`Your total amount is $${total_price}.\nThank you for your purchase on BAMAZON!`.green);
+        // updates database
+        updateStock(buy_quantity, item_id);
+    } else {
+        console.log(`Insufficient quantity on stock!\nOnly ${on_stock} items on stock!`.red);
+        connection.end();
+    }
+}
+
+// updates stock_quantity in the database
+function updateStock(quantity, item_id) {
+    var query = "UPDATE products SET stock_quantity = stock_quantity - ? WHERE ?";
+    connection.query(
+        query,
+        [
+            quantity,
+            {
+                item_id: item_id
+            }
+        ],
+        function (error) {
+            if (error) throw error;
+            console.log("DB was succefully updated!");
+            connection.end();
+        });
+}
---
+++ kamazonManager.js
@@ -0,0 +1,224 @@
+// Dependencies
+var mysql = require("mysql");
+var wrap = require("word-wrap");
+var Table = require("cli-table");
+var inquirer = require("inquirer");
+var colors = require("colors");
+
+// sets connection param for database connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    // sets username
+    user: "root",
+    // sets password
+    password: "Happy1day",
+    // sets current database
+    database: "kamazon_db"
+});
+
+// makes connection with the server
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    chooseMenu();
+});
+
+// manager chooses a from a menu list
+function chooseMenu() {
+    inquirer
+        .prompt({
+            name: "menu",
+            type: "list",
+            message: "Please select a menu option?",
+            choices: [
+                "View Products for Sale",
+                "View Low Inventory",
+                "Add to Inventory",
+                "Add New Product",
+                "Exit Manager View"
+            ]
+        })
+        .then(function (answer) {
+            // console.log(answer);
+            switch (answer.menu) {
+                case "View Products for Sale":
+                    displayItemsForSale();
+                    break;
+
+                case "View Low Inventory":
+                    displayLowInventory();
+                    break;
+
+                case "Add to Inventory":
+                    displayItemsForSale(addToInvetory);
+                    break;
+
+                case "Add New Product":
+                    askQuestions();
+                    break;
+
+                case "Exit Manager View":
+                    exit();
+                    break;
+            }
+        })
+}
+// displays low invetory when requested
+function displayItemsForSale(func) {
+    connection.query(
+        "SELECT item_id, product_name, price, stock_quantity, department_name " +
+        "FROM products " +
+        "WHERE price > 0;", function (err, result) {
+
+        if (err) throw err;
+        // gets and builds the table header
+        var obj = result[0];
+        var header = [];
+        for (var prop in obj) {
+            header.push(prop);
+        }
+
+        // instantiate 
+        var table = new Table({
+            head: header,
+            colWidths: [15, 55, 10, 5, 20]
+        });
+
+        // gets and sets the data in the table
+        var item_ids = [];
+        for (var i = 0; i < result.length; i++) {
+            item_ids.push(result[i].item_id);
+            table.push([result[i].item_id, wrap(result[i].product_name), result[i].price.toFixed(2),
+            result[i].stock_quantity, result[i].department_name]);
+        }
+        var output = table.toString();
+        console.log(output);
+        if (func) {
+            func(item_ids);
+        } else {
+            chooseMenu();
+        }
+    });
+}
+
+// displays low inventory (stock lower or equal than 5) when requested 
+function displayLowInventory() {
+    connection.query(
+        "SELECT item_id, stock_quantity " +
+        "FROM products " +
+        "WHERE stock_quantity <= 5;", function (err, result) {
+
+        // console.log(err)
+        var obj = result[0];
+        var header = [];
+        for (var prop in obj) {
+            header.push(prop);
+        }
+
+        // instantiate 
+        var table = new Table({
+            head: header,
+            colWidths: [15, 10]
+        });
+
+        for (var i = 0; i < result.length; i++) {
+            table.push([result[i].item_id, result[i].stock_quantity]);
+        }
+        var output = table.toString();
+        console.log(output);
+        chooseMenu();
+    });
+}
+
+// funtion adds to inventory
+function addToInvetory(list) {
+    inquirer
+        .prompt([{
+            name: "action",
+            type: "list",
+            message: "Select item:",
+            choices: list
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "Update quantity with:",
+        }])
+        .then(function (answer) {
+            updateQuantity(answer.action, answer.quantity);
+        })
+}
+
+function updateQuantity(item, quant) {
+    var query = "UPDATE products SET stock_quantity = stock_quantity + ? WHERE ?"
+    connection.query(
+        query,
+        [
+            quant,
+            {
+                item_id: item
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log("DB has been updated!".green);
+            chooseMenu();
+        }
+    );
+}
+
+// prompts for adding new item
+function askQuestions() {
+    inquirer
+        .prompt([{
+            name: "id",
+            message: "Input Item ID: "
+        },
+        {
+            name: "name",
+            message: "Input Product Name: "
+        },
+        {
+            name: "department",
+            type: "list",
+            message: "Select Department",
+            choices: ["Electronics", "Books", "Clothing", "Shoes", "Handmade"]
+        },
+        {
+            name: "price",
+            message: "Input Product Cost: "
+        },
+        {
+            name: "stock",
+            message: "Input Stock Quantity: "
+        }])
+        .then(function (answer) {
+            // adds new item to database
+            addNewItem(answer.id, answer.name, answer.department, answer.price, answer.stock);
+        });
+}
+
+// adds new item to product table
+function addNewItem(id, name, department, price, stock) {
+    var query = "INSERT INTO products SET ?"
+    connection.query(query,
+        {
+            item_id: id,
+            product_name: name,
+            department_name: department,
+            price: price,
+            stock_quantity: stock
+        },
+        function (err, res) {
+            if (err) console.log(err)
+            console.log(`New item ${id} is added to DB!`.green);
+            chooseMenu();
+        }
+    )
+}
+
+function exit() {
+    connection.end();
+    process.exit(-1);
+}
